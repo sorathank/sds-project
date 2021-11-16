0c26fe6def6f5c9249f2607158fd8972e307fb2b
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2021-11-16 14:39:16 +0700

    Declare Variable's names

---
+++ src/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+}
+variable "availability_zone" {
+}
+variable "ami" {
+}  
+variable "bucket_name" {
+}
+variable "database_name" {
+} 
+variable "database_user" {
+}  
+variable "database_pass" {
+} 
+variable "admin_user" {
+}
+variable "admin_pass" {
+}
+variable "PUBLIC_KEY_SSH" {
+}
