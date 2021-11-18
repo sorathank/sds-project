843238d728ee6128850180bb6fcb9a621af49bc1
Sorathan Kritveeranant <sorathan.krit@gmail.com>
2021-11-18 17:34:38 +0700

    Add network interfaces with no security group

--- networkInterface.tf
+++ networkInterface.tf
@@ -1,10 +1,34 @@
-/*resource "aws_network_interface" "public" {
-  subnet_id       = aws_subnet.public.id
-  private_ips     = ["10.0.0.50"]
-  security_groups = [aws_security_group.web.id]
-
-  attachment {
-    instance     = aws_instance.test.id
-    device_index = 1
+resource "aws_network_interface" "app_public" {
+  subnet_id       = aws_subnet.app_public.id
+  # security_groups = [aws_security_group.web.id]
+  tags = {
+    Name = "app_public_network_interface"
   }
-}*/
+}
+
+resource "aws_network_interface" "app_private" {
+  subnet_id       = aws_subnet.app_db_private
+  # security_groups = [aws_security_group.web.id]
+
+  tags = {
+    Name = "app_private_network_interface"
+  }
+}
+
+resource "aws_network_interface" "db_app_private" {
+  subnet_id       = aws_subnet.app_db_private
+  # security_groups = [aws_security_group.web.id]
+
+  tags = {
+    Name = "db_to_app_network_interface"
+  }
+}
+
+resource "aws_network_interface" "db_private" {
+  subnet_id       = aws_subnet.app_db_private
+  # security_groups = [aws_security_group.web.id]
+
+  tags = {
+    Name = "db_to_app_network_interface"
+  }
+}
